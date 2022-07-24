610e33bf24d2626c2b6e3fab181f1d6d8bca9abb
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-07-24 19:08:41 +0200

    Recover last commit

--- Data/ApplicatonDbContext.cs
+++ Data/ApplicatonDbContext.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data;
-internalclass ApplicatonDbContext
+public class ApplicatonDbContext: IdentityDbContext<IdentityUser>
 {
+    public ApplicatonDbContext(DbContextOptions<ApplicatonDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+    public DbSet<IdentityUser> ApplicationUser { get; set; }
 }
